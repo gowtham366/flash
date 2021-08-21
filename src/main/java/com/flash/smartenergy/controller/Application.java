153655594cb95dc6be6eba7362b0d6fc5a52ec96
Gowtham C <gowtham366.vpi@gmail.com>
2021-08-21 22:31:05 +0530

    added user name and password to property

---
+++ src/main/java/com/flash/smartenergy/controller/Application.java
@@ -0,0 +1,13 @@
+package com.flash.smartenergy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class Application {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "Smart energy application";
+    }
+}
